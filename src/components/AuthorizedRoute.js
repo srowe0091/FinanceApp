419b51d5b037faf1d08d38b499e1c08df7bc2ad9
mrowe009 <mrowe009@fiu.edu>
2020-01-02 02:04:10 -0500

    added authorized route component

---
+++ src/components/AuthorizedRoute.js
@@ -0,0 +1,52 @@
+import React, { createElement } from 'react'
+import PropTypes from 'prop-types'
+import { IonPage, IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem } from '@ionic/react'
+import { Route, Redirect } from 'react-router-dom'
+
+import { useAuthentication } from 'modules/authentication/hooks'
+import routes from 'routes'
+
+const Menu = () => (
+  <IonMenu contentId="main">
+    <IonHeader>
+      <IonToolbar>
+        <IonTitle>Menu</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+    <IonContent>
+      <IonList>
+        <IonItem>Menu Item</IonItem>
+        <IonItem>Menu Item</IonItem>
+        <IonItem>Menu Item</IonItem>
+        <IonItem>Menu Item</IonItem>
+        <IonItem>Menu Item</IonItem>
+      </IonList>
+    </IonContent>
+  </IonMenu>
+)
+
+export const AuthorizedRoute = ({ path, component }) => {
+  const { isAuthenticated } = useAuthentication()
+
+  if (!isAuthenticated) {
+    return <Redirect to={routes.login} />
+  }
+
+  return (
+    <Route path={path}>
+      {props => (
+        <>
+          <Menu />
+          <IonPage id="main" color="primary">
+            {createElement(component, props)}
+          </IonPage>
+        </>
+      )}
+    </Route>
+  )
+}
+
+AuthorizedRoute.propTypes = {
+  path: PropTypes.string,
+  component: PropTypes.elementType
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+export * from './AuthorizedRoute'
 export * from './FullPageLoader'
 export * from './Logo'
 export * from './Transaction'
