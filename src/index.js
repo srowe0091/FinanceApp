b785a64eb5e614b8014fd82d4f4a1354df0bbbe9
mrowe009 <mrowe009@fiu.edu>
2020-01-02 02:03:41 -0500

    added ionic css files and added own theme css file

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import App from './App'
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css'
+import '@ionic/react/css/structure.css'
+import '@ionic/react/css/normalize.css'
+import '@ionic/react/css/typography.css'
+import './styles/theme.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
