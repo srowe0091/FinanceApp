475eaf40915be9a94b609aee7a9183da7458f7c4
mrowe009 <mrowe009@fiu.edu>
2020-01-05 23:56:33 -0500

    added links for routes

--- src/modules/drawer/Drawer.js
+++ src/modules/drawer/Drawer.js
@@ -1,18 +1,54 @@
-import React from 'react'
-import { IonMenu, IonContent, IonList, IonItem } from '@ionic/react'
+import React, { useCallback } from 'react'
+import { menuController } from '@ionic/core'
+import { IonMenu, IonContent, IonList, IonItem, IonLabel, IonIcon } from '@ionic/react'
+import { createUseStyles } from 'react-jss'
 
+import { home, person, settings} from 'ionicons/icons'
+
+import routes from 'routes'
 import { PAGE_ID } from 'utils'
 
-export const DrawerMenu = () => (
-  <IonMenu contentId={PAGE_ID}>
-    <IonContent>
-      <IonList>
-        <IonItem>Menu Item</IonItem>
-        <IonItem>Menu Item</IonItem>
-        <IonItem>Menu Item</IonItem>
-        <IonItem>Menu Item</IonItem>
-        <IonItem>Menu Item</IonItem>
-      </IonList>
-    </IonContent>
-  </IonMenu>
-)
+const drawerLinks = [
+  {
+    icon: home,
+    label: 'Home',
+    route: routes.home
+  },
+  {
+    icon: person,
+    label: 'Profile',
+    route: routes.profile
+  },
+  {
+    icon: settings,
+    label: 'Admin',
+    route: routes.admin
+  }
+]
+
+const useDrawerMenuStyles = createUseStyles(theme => ({
+  icon: {
+    marginRight: theme.spacing(3)
+  }
+}))
+
+export const DrawerMenu = () => {
+  const classes = useDrawerMenuStyles()
+
+  const closeDrawer = useCallback(() => menuController.close(), [])
+
+  return (
+    <IonMenu contentId={PAGE_ID}>
+      <IonContent>
+        <IonList lines="full">
+          {drawerLinks.map(r => (
+            <IonItem key={r.label} routerLink={r.route} onClick={closeDrawer}>
+              <IonLabel>{r.label}</IonLabel>
+              <IonIcon slot="start" className={classes.icon} icon={r.icon}>{r.label}</IonIcon>
+            </IonItem>
+          ))}
+        </IonList>
+      </IonContent>
+    </IonMenu>
+  )
+}
