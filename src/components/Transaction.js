2e0e0dc6895f1c9b3acc8d3bfeda8a701bcd34e8
mrowe009 <mrowe009@fiu.edu>
2020-01-24 01:12:22 -0500

    added date and cleaned up styling

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { createUseStyles } from 'react-jss'
+import { parseISO, format } from 'date-fns'
 
 const useTransactionStyles = createUseStyles({
   transaction: {
     marginBottom: 16,
     padding: 12,
     display: 'flex',
+    alignItems: 'center',
     justifyContent: 'space-between',
+    flexDirection: 'row',
     borderRadius: 8,
     color: 'var(--white)',
     backgroundColor: 'var(--themeGray1)',
@@ -15,12 +18,14 @@ const useTransactionStyles = createUseStyles({
   }
 })
 
-export const TransactionEntry = ({ key, amount, description }) => {
+export const TransactionEntry = ({ key, amount, description, dateCreated }) => {
   const classes = useTransactionStyles()
   return (
     <div key={key} className={classes.transaction}>
-      <p>{description}</p>
-
+      <div>
+        <p>{description}</p>
+        <p color="textSecondary" variant="caption">{format(parseISO(dateCreated), 'M/d/y')}</p>
+      </div>
       <p>${(amount / 100).toFixed(2)}</p>
     </div>
   )
@@ -29,5 +34,6 @@ export const TransactionEntry = ({ key, amount, description }) => {
 TransactionEntry.propTypes = {
   key: PropTypes.string,
   amount: PropTypes.number,
-  description: PropTypes.string
+  description: PropTypes.string,
+  dateCreated: PropTypes.string
 }
