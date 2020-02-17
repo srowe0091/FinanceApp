62781fd38bb72771f4dd1bb6c3ce56ba510c0f19
mrowe009 <mrowe009@fiu.edu>
2020-02-17 17:55:05 -0500

    cleaned up transaction component

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,39 +1,51 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { createUseStyles } from 'react-jss'
-import { parseISO, format } from 'date-fns'
+import { IonItem, IonLabel, IonCheckbox } from '@ionic/react';
 
-const useTransactionStyles = createUseStyles({
+
+import { formatDate } from 'utils/normalizer'
+
+const useTransactionStyles = createUseStyles(theme => ({
   transaction: {
-    marginBottom: 16,
-    padding: 12,
+    marginBottom: theme.spacing(2),
+    borderRadius: theme.spacing(1),
+    boxShadow: '0px 2px 5px -2px var(--black)',
+    '--background': 'var(--themeGray1)'
+  },
+  label: {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'space-between',
-    flexDirection: 'row',
-    borderRadius: 8,
-    color: 'var(--white)',
-    backgroundColor: 'var(--themeGray1)',
-    boxShadow: '0px 2px 5px -2px var(--black)'
+  },
+  checkbox: {
+    marginRight: theme.spacing(2)
   }
-})
+}))
 
-export const TransactionEntry = ({ key, amount, description, dateCreated }) => {
+export const TransactionEntry = ({ _id, amount, description, dateCreated, onCheckboxClick, checked }) => {
   const classes = useTransactionStyles()
   return (
-    <div key={key} className={classes.transaction}>
-      <div>
-        <p>{description}</p>
-        <p color="textSecondary" variant="caption">{format(parseISO(dateCreated), 'M/d/y')}</p>
-      </div>
-      <p>${(amount / 100).toFixed(2)}</p>
-    </div>
+    <IonItem className={classes.transaction}>
+      <IonLabel color="light">
+        <span className={classes.label}>
+          <span>
+            <p>{description}</p>
+            <p color="textSecondary" variant="caption">{formatDate(dateCreated)}</p>
+          </span>
+          <p>${(amount / 100).toFixed(2)}</p>
+        </span>
+      </IonLabel>
+      {onCheckboxClick && <IonCheckbox slot="start" className={classes.checkbox} checked={checked} onIonChange={onCheckboxClick(_id)} />}
+    </IonItem>
   )
 }
 
 TransactionEntry.propTypes = {
-  key: PropTypes.string,
+  _id: PropTypes.string,
   amount: PropTypes.number,
   description: PropTypes.string,
-  dateCreated: PropTypes.string
+  dateCreated: PropTypes.string,
+  checked: PropTypes.bool,
+  onCheckboxClick: PropTypes.func
 }
