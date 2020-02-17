686bfe402fafd1310a918c29cd97529a0fa568f0
mrowe009 <mrowe009@fiu.edu>
2020-02-17 17:55:27 -0500

    added admin view

---
+++ src/modules/admin/admin.gql.js
@@ -0,0 +1,26 @@
+import gql from 'graphql-tag'
+
+import { TransactionFragment } from 'modules/transaction'
+
+export const GroupTransactions = gql`
+  query GroupTransactions {
+    admin {
+      groupTransactions {
+        owner
+        ...TransactionFragment
+      }
+    }
+  }
+  ${TransactionFragment}
+`
+
+export const PayTransactions = gql`
+  mutation PayTransactions($transactionIds: [ID]!) {
+    admin {
+      payTransactions(transactionIds: $transactionIds) {
+        _id
+        
+      }
+    }
+  }
+`
---
+++ src/modules/admin/views/AdminView.js
@@ -0,0 +1,82 @@
+import React, { useCallback, useState, useMemo, Fragment } from 'react'
+import PropTypes from 'prop-types'
+import { IonContent, IonText, useIonViewWillLeave } from '@ionic/react'
+import { createUseStyles } from 'react-jss'
+import { useQuery, useMutation } from '@apollo/react-hooks'
+import get from 'lodash/fp/get'
+import map from 'lodash/fp/map'
+import concat from 'lodash/fp/concat'
+import reject from 'lodash/fp/reject'
+import groupBy from 'lodash/fp/groupBy'
+import includes from 'lodash/fp/includes'
+
+import { Button } from 'elements'
+import { Toolbar, TransactionEntry, FullPageLoader } from 'components'
+import routes from 'routes'
+import { GroupTransactions, PayTransactions } from '../admin.gql'
+
+const useAdminViewStyles = createUseStyles(theme => ({
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+    margin: theme.spacing(0, 2)
+  },
+  transactions: {
+    flex: 1,
+    overflow: 'auto',
+    paddingTop: theme.spacing(2)
+  },
+  headers: {
+    marginBottom: theme.spacing(1)
+  },
+  button: {
+    margin: theme.spacing(2, 0)
+  },
+}))
+
+const Admin = ({ history }) => {
+  const classes = useAdminViewStyles()
+  const [transactionIds, handleIds] = useState([])
+  const [payTransaction, { loading: ptLoading }] = useMutation(PayTransactions, {
+    variables: { transactionIds },
+    onCompleted: () => history.push(routes.home)
+  })
+  const { data, loading } = useQuery(GroupTransactions)
+  const checkboxClick = useCallback(id => e => handleIds(_ids => e.target.checked ? concat(_ids)(id) : reject(_id => _id === id)(_ids)), [])
+
+  const transactions = useMemo(() => groupBy('owner')(get('admin.groupTransactions')(data)), [data])
+
+  useIonViewWillLeave(() => handleIds([]))
+
+  if (loading) {
+    return <FullPageLoader />
+  }
+
+  return (
+    <>
+      <Toolbar color="primary" />
+      <IonContent color="dark">
+        <div className={classes.wrapper}>
+          <div className={classes.transactions}>
+            {Object.keys(transactions).map(email => (
+              <Fragment key={email}>
+                <IonText color="textSecondary">
+                  <h5 className={classes.headers}>{email}</h5>
+                </IonText>
+                {map(t => <TransactionEntry key={t._id} onCheckboxClick={checkboxClick} checked={includes(t._id)(transactionIds)} {...t} />)(transactions[email])}
+              </Fragment>
+            ))}
+          </div>
+          <Button type="button" className={classes.button} disabled={ptLoading} loading={ptLoading} onClick={payTransaction}>Pay</Button>
+        </div>
+      </IonContent>
+    </>
+  )
+}
+
+Admin.propTypes = {
+  history: PropTypes.object
+}
+
+export default Admin
