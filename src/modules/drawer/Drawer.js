46046c19a3f91dc7cd27198b569790e862ad8c68
mrowe009 <mrowe009@fiu.edu>
2020-02-23 11:28:47 -0500

    styled drawer; conditional routing

--- src/modules/drawer/Drawer.js
+++ src/modules/drawer/Drawer.js
@@ -1,17 +1,29 @@
 import React, { useCallback, useMemo } from 'react'
 import { menuController } from '@ionic/core'
-import { IonMenu, IonContent, IonList, IonItem, IonLabel, IonIcon } from '@ionic/react'
+import { IonMenu, IonContent, IonItem, IonLabel, IonIcon } from '@ionic/react'
 import { createUseStyles } from 'react-jss'
+import { useLocation } from "react-router-dom";
 import has from 'lodash/fp/has'
 import filter from 'lodash/fp/filter'
 
-import { home, person, settings} from 'ionicons/icons'
+import { home, person, settings, personCircle, logOutOutline } from 'ionicons/icons'
 
 import routes from 'routes'
 import { PAGE_ID } from 'utils'
-import { useUser } from 'modules/authentication'
+import { useUser, useAuthentication } from 'modules/authentication'
 
 const useDrawerMenuStyles = createUseStyles(theme => ({
+  user: {
+    height: '150px',
+    padding: theme.spacing(1, 2),
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    background: 'var(--themeGray1)'
+  },
+  userIcon: {
+    fontSize: '48px'
+  },
   icon: {
     marginRight: theme.spacing(3)
   }
@@ -19,7 +31,9 @@ const useDrawerMenuStyles = createUseStyles(theme => ({
 
 export const DrawerMenu = () => {
   const classes = useDrawerMenuStyles()
-  const { role } = useUser()
+  const location = useLocation()
+  const { role, email } = useUser()
+  const { handleLogout, isAuthenticated } = useAuthentication()
   const closeDrawer = useCallback(() => menuController.close(), [])
   const drawerLinks = useMemo(() => {
     const links = [
@@ -43,19 +57,35 @@ export const DrawerMenu = () => {
     return filter(link => has('condition')(link) ? link.condition : true)(links)
   }, [role])
 
-
+  if (!isAuthenticated) {
+    return null
+  }
 
   return (
     <IonMenu contentId={PAGE_ID}>
-      <IonContent>
-        <IonList lines="full">
-          {drawerLinks.map(r => (
-            <IonItem key={r.label} routerLink={r.route} onClick={closeDrawer}>
-              <IonLabel>{r.label}</IonLabel>
-              <IonIcon slot="start" className={classes.icon} icon={r.icon}>{r.label}</IonIcon>
-            </IonItem>
-          ))}
-        </IonList>
+      <IonContent className={classes.drawer} color="dark">
+        <div className={classes.user}>
+          <IonIcon className={classes.userIcon} icon={personCircle} />
+          {email}
+        </div>
+        {drawerLinks.map(r => (
+          <IonItem
+            key={r.label}
+            detail
+            lines="none"
+            color={location.pathname === r.route ? "tertiary" : "dark"}
+            onClick={closeDrawer}
+            className={classes.item}
+            {...(location.pathname !== r.route) && { routerLink: r.route }}
+          >
+            <IonIcon slot="start" className={classes.icon} icon={r.icon} />
+            <IonLabel>{r.label}</IonLabel>
+          </IonItem>
+        ))}
+        <IonItem button lines="none" color="dark" onClick={handleLogout}>
+          <IonIcon slot="start" className={classes.icon} icon={logOutOutline} />
+          <IonLabel>Logout</IonLabel>
+        </IonItem>
       </IonContent>
     </IonMenu>
   )
