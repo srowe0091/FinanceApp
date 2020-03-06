5479486308a44716b2538e1333ce42ee68c47e22
mrowe009 <mrowe009@fiu.edu>
2020-03-06 01:56:40 -0500

    created reducer for authentication to make it easier to manage; refactored, cleaned up implementation

--- src/modules/authentication/hooks.js
+++ src/modules/authentication/hooks.js
@@ -1,8 +1,8 @@
-import { useState, useCallback, useContext, useEffect } from 'react'
+import { useCallback, useContext, useEffect } from 'react'
 import { useApolloClient } from '@apollo/react-hooks'
-import set from 'lodash/fp/set'
 
 import { AuthContext } from './context'
+import { useAuthReducer } from './reducer'
 
 const checkErrors = response => {
   if (!response.ok) {
@@ -12,68 +12,82 @@ const checkErrors = response => {
 }
 
 export const useInitializeAuth = () => {
-  const [state, handleState] = useState({ isLoading: true, isAuthenticated: false, user: {} })
+  const [state, dispatch] = useAuthReducer()
   const client = useApolloClient()
+
   useEffect(() => {
     const _sessionId = localStorage.getItem('session')
     if (_sessionId) {
       fetch(`${process.env.REACT_APP_SERVER_URL}/authenticate/session`, {
         headers: { Authorization: _sessionId }
-      })
-        .then(checkErrors)
-        .then(session => handleState({ isLoading: false, isAuthenticated: true, user: JSON.parse(session) }))
-        .catch(() => handleState(set('isLoading')(false)))
+      }).then(checkErrors)
+        .then(session => {
+          const _session = JSON.parse(session)
+          if (!_session.dueDate || !_session.allowance) {
+            dispatch({ type: 'COMPLETE_PROFILE', payload: _session })
+            return
+          }
+          dispatch({ type: 'LOGIN', payload: _session })
+        })
+        .catch(() => dispatch({ type: 'STOP_LOADING' }))
     } else {
-      handleState(set('isLoading')(false))
+      dispatch({ type: 'STOP_LOADING' })
     }
-  }, [])
+  }, [dispatch])
 
   const handleLogin = useCallback(
-    (email, password) =>
-      fetch(`${process.env.REACT_APP_SERVER_URL}/authenticate`, {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      .then(checkErrors)
+    (email, password) => fetch(`${process.env.REACT_APP_SERVER_URL}/authenticate`, {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(checkErrors)
       .then(session => {
         const _session = JSON.parse(session)
         localStorage.setItem('session', _session.sessionId)
-        handleState({ isLoading: false, isAuthenticated: true, user: _session })
+        if (!_session.dueDate || !_session.allowance) {
+          dispatch({ type: 'COMPLETE_PROFILE', payload: _session })
+          return
+        }
+        dispatch({ type: 'LOGIN', payload: _session })
       }),
-    []
+    [dispatch]
   )
 
   const handleLogout = useCallback(
-    () =>
-      fetch(`${process.env.REACT_APP_SERVER_URL}/authenticate`, {
-        method: 'DELETE',
-        headers: { Authorization: localStorage.getItem('session') }
-      })
-      .then(checkErrors)
+    () => fetch(`${process.env.REACT_APP_SERVER_URL}/authenticate`, {
+      method: 'DELETE',
+      headers: { Authorization: localStorage.getItem('session') }
+    }).then(checkErrors)
       .then(() => client.clearStore())
       .then(() => {
         localStorage.removeItem('session')
-        handleState(set('isAuthenticated')(false))
+        dispatch({ type: 'LOGOUT' })
       }),
-    [client]
+    [client, dispatch]
+  )
+
+  const finishProfile = useCallback(
+    data => dispatch({ type: 'FINISHED_PROFILE', payload: data }),
+    [dispatch]
   )
 
   return {
     ...state,
     handleLogin,
-    handleLogout
+    handleLogout,
+    finishProfile
   }
 }
 
 export const useAuthentication = () => {
-  const { isAuthenticated, handleLogin, handleLogout } = useContext(AuthContext)
-  return { isAuthenticated, handleLogin, handleLogout }
+  const { isAuthenticated, handleLogin, handleLogout, finishProfile, requireProfileUpdate } = useContext(AuthContext)
+  return { isAuthenticated, handleLogin, handleLogout, finishProfile, requireProfileUpdate }
 }
 
 export const useUser = () => {
   const { user } = useContext(AuthContext)
+  console.log(user)
   return user
 }
---
+++ src/modules/authentication/reducer.js
@@ -0,0 +1,33 @@
+import { useReducer } from 'react'
+import pick from 'lodash/fp/pick'
+import merge from 'lodash/fp/merge'
+
+const initialState = {
+  isLoading: true,
+  isAuthenticated: false,
+  requireProfileUpdate: false,
+  user: {}
+}
+
+const sessionProps = pick(['email', 'role', 'allowance', 'dueDate'])
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'LOGIN':
+      return Object.assign({}, state, { isLoading: false, isAuthenticated: true, user: sessionProps(action.payload) })
+    case 'LOGOUT':
+      return Object.assign({}, state, { isAuthenticated: false, user: {} })
+    case 'STOP_LOADING':
+      return Object.assign({}, state, { isLoading: false })
+    case 'COMPLETE_PROFILE':
+      return Object.assign({}, state, { isLoading: false, requireProfileUpdate: true, user: sessionProps(action.payload) })
+    case 'FINISHED_PROFILE':
+      return Object.assign({}, state, { requireProfileUpdate: false, isAuthenticated: true, user: sessionProps(merge(state.user)(action.payload)) })
+    default:
+      return state
+  }
+}
+
+export const useAuthReducer = () => {
+  return useReducer(reducer, initialState)
+}
