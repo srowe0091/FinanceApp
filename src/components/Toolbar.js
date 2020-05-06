0238d16dbbe33656d83fd65a926312aa8107be72
mrowe009 <mrowe009@fiu.edu>
2020-05-06 02:16:54 -0400

    fixed toolbar fading transition when scrolling

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,48 +1,45 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import clsx from 'clsx'
 import { IonHeader, IonMenuButton, IonToolbar, IonButtons, IonBackButton, IonTitle } from '@ionic/react'
 import { createUseStyles } from 'react-jss'
 
 const useToolbarStyles = createUseStyles(theme => ({
-  '@keyframes fadeIn': {
-    from: { backgroundColor: 'var(--alpha0)' },
-    to: { backgroundColor: 'var(--ion-color-primary)' }
-  },
-  '@keyframes fadeOut': {
-    from: { backgroundColor: 'var(--ion-color-primary)' },
-    to: { backgroundColor: 'var(--alpha0)' }
-  },
   header: {
     '&:after': {
-      content: 'none'
+      content: transition => (transition ? '""' : 'none')
     }
+  },
+  toolbar: {
+    backgroundColor: 'var(--alpha0)',
+    transition: 'background-color 250ms linear'
+  },
+  fadeIn: {
+    backgroundColor: 'var(--ion-color-dark)'
   },
   icon: {
     margin: theme.spacing(0, 1)
   }
 }))
 
-export const Toolbar = ({ children, title, back, extraToolbar, color }) => {
-  const classes = useToolbarStyles()
+export const Toolbar = ({ children, title, back, transition }) => {
+  const classes = useToolbarStyles(transition)
   return (
     <IonHeader className={classes.header}>
-      <IonToolbar color={color}>
+      <IonToolbar className={clsx(classes.toolbar, transition && classes.fadeIn)}>
         <IonButtons className={classes.icon} slot="start">
           {back ? <IonBackButton /> : <IonMenuButton />}
         </IonButtons>
         {title && <IonTitle>{title}</IonTitle>}
         {children}
       </IonToolbar>
-      {extraToolbar}
     </IonHeader>
   )
 }
 
 Toolbar.propTypes = {
   children: PropTypes.node,
-  color: PropTypes.string,
   title: PropTypes.string,
   back: PropTypes.bool,
-  transition: PropTypes.bool,
-  extraToolbar: PropTypes.node
+  transition: PropTypes.bool
 }
