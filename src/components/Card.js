fae4f4b02ecc1bfd6b861ee166d37c3955924d75
mrowe009 <mrowe009@fiu.edu>
2020-05-10 17:31:46 -0400

    added default tag label for cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,10 @@
 import React, { useMemo } from 'react'
 import PropTypes from 'prop-types'
 import clsx from 'clsx'
+import { IonText } from '@ionic/react'
 import { createUseStyles } from 'react-jss'
 
-import { AspectRatio, Button } from 'elements'
-import { IonText } from '@ionic/react'
+import { AspectRatio } from 'elements'
 import { formatDate } from 'utils'
 
 const cardTitle = (spacing, content) => ({
@@ -17,6 +17,19 @@ const cardTitle = (spacing, content) => ({
   top: spacing,
   position: 'absolute'
 })
+
+const defaultTagStyling = {
+  width: '45%',
+  height: '95%',
+  display: 'block',
+  top: '0px',
+  position: 'absolute',
+  clip: 'rect(1px, 25px, 20px, 1px)',
+  background: 'transparent',
+  content: '""',
+  borderBottomRightRadius: '100%',
+  boxShadow: '0px 0px 0px 20px var(--gray7)'
+}
 
 const useStyles = createUseStyles(theme => ({
   container: {
@@ -43,34 +56,59 @@ const useStyles = createUseStyles(theme => ({
     bottom: theme.spacing(2),
     position: 'absolute'
   },
+  default: {
+    padding: theme.spacing(0.1, 1.5, 0.25),
+    top: '2px',
+    left: '50%',
+    transform: 'translateX(-50%) translateY(-100%)',
+    position: 'absolute',
+    backgroundColor: 'var(--gray7)',
+    borderRadius: theme.spacing(2, 2, 0, 0),
+    zIndex: -1,
+    '&::before': {
+      ...defaultTagStyling,
+      left: '5px',
+      transform: 'translateY(0) translateX(-100%)'
+    },
+    '&::after': {
+      ...defaultTagStyling,
+      right: '5px',
+      transform: 'translateY(0) translateX(100%) scaleX(-1)'
+    }
+  },
   VISA: {
     background: 'linear-gradient(135deg, #6d87e7 0%, #6bcbe1 100%)',
     boxShadow: '0px 10px 20px -5px #2d97bd',
-    '&::before': cardTitle(theme.spacing(1), 'VISA')
+    '&::before': cardTitle(theme.spacing(2), 'VISA')
   },
   DISCOVER: {
     background: 'linear-gradient(135deg, #dd581c 0%, #fba321 100%)',
     boxShadow: '0px 10px 20px -5px #dd581c',
-    '&::before': cardTitle(theme.spacing(1), 'DISCOVER')
+    '&::before': cardTitle(theme.spacing(2), 'DISCOVER')
   },
   MASTERCARD: {
     background: 'linear-gradient(135deg, #e52e29 0%, #f89d2d 100%)',
     boxShadow: '0px 10px 20px -5px #f5742d',
-    '&::before': cardTitle(theme.spacing(1), 'MASTERCARD')
+    '&::before': cardTitle(theme.spacing(2), 'MASTERCARD')
   },
   AMERICAN_EXPRESS: {
     background: 'linear-gradient(135deg, #01aae3 0%, #27256c 100%)',
     boxShadow: '0px 10px 20px -5px #2e1c92',
-    '&::before': cardTitle(theme.spacing(1), 'AMEX')
+    '&::before': cardTitle(theme.spacing(2), 'AMEX')
   }
 }))
 
-export const Card = ({ className, name = 'Name of Card', type, dueDate = 'XX', createdAt }) => {
+export const Card = ({ className, type, createdAt, isDefault, dueDate = 'XX', name = 'Name of Card' }) => {
   const classes = useStyles(type)
   const _createdDate = useMemo(() => (createdAt ? formatDate(createdAt, 'MM/YYYY') : 'XX/XXXX'), [createdAt])
   return (
     <AspectRatio className={className} ratio={7 / 4} maxWidth={350}>
       <div className={clsx(classes.container, classes[type])}>
+        {isDefault && (
+          <p variant="caption" className={classes.default}>
+            Default
+          </p>
+        )}
         <IonText>{name}</IonText>
 
         <IonText align="left">
@@ -89,11 +127,11 @@ export const Card = ({ className, name = 'Name of Card', type, dueDate = 'XX', c
           <p display="inline">{_createdDate}</p>
         </IonText>
 
-        {createdAt && (
+        {/* {createdAt && (
           <Button color="medium" size="small" className={classes.details}>
             Details
           </Button>
-        )}
+        )} */}
       </div>
     </AspectRatio>
   )
@@ -101,8 +139,9 @@ export const Card = ({ className, name = 'Name of Card', type, dueDate = 'XX', c
 
 Card.propTypes = {
   className: PropTypes.string,
-  name: PropTypes.string,
   type: PropTypes.string,
-  dueDate: PropTypes.number,
-  createdAt: PropTypes.string
+  createdAt: PropTypes.string,
+  isDefault: PropTypes.bool,
+  name: PropTypes.string,
+  dueDate: PropTypes.number
 }
