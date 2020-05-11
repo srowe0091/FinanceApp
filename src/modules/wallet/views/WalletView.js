c596a48772a5218c7c73e50a8dd4ba453d602976
mrowe009 <mrowe009@fiu.edu>
2020-05-10 23:03:35 -0400

    finished creating cards and assigning cards as defaults

--- src/modules/wallet/views/WalletView.js
+++ src/modules/wallet/views/WalletView.js
@@ -1,49 +1,80 @@
-import React from 'react'
-import { IonSlides, IonSlide, IonModal } from '@ionic/react'
+import React, { useState, useCallback, useRef, useMemo } from 'react'
+import { IonSlides, IonSlide, IonButtons, IonIcon, IonItem, IonButton } from '@ionic/react'
+import { ellipsisVertical } from 'ionicons/icons'
 import useToggle from 'react-use/lib/useToggle'
 import map from 'lodash/fp/map'
+import set from 'lodash/fp/set'
 
 import { NewCardView } from './NewCardView'
-import { useGetWallet, useWalletStyles } from '../util'
-import { Fab } from 'elements'
-import { Card } from 'components'
+import { useWalletStyles } from '../util'
+import { useWallet } from '../hooks'
+import { Card, Modal, Popover } from 'components'
 import { ToolbarContent } from 'template'
+import { hash } from 'utils'
+import { useUpdatePreferences } from 'modules/preferences'
 
 const slideOpts = {
-  slidesPerView: 1.3,
+  slidesPerView: 1.2,
   initialSlide: 0,
   spaceBetween: 20,
   centeredSlides: true
 }
 
-const ProfileView = () => {
+const Wallet = () => {
   const classes = useWalletStyles()
-  const { data, loading } = useGetWallet()
+  const ref = useRef()
+  const { cards, defaultCard } = useWallet()
   const [addCardModal, toggleAddCard] = useToggle(false)
+  const [optionsDropdown, setShowPopover] = useState({})
+  const key = useMemo(() => hash(JSON.stringify(cards)), [cards])
 
-  if (loading) {
-    return null
-  }
+  const openPopover = useCallback(async e => {
+    e.persist()
+    const isFirst = await ref.current.isBeginning()
+    setShowPopover({ event: e.nativeEvent, disableOption: isFirst })
+  }, [])
+  const closePopover = useCallback(() => setShowPopover({}), [])
+
+  const [udpatePreferences] = useUpdatePreferences()
+
+  const setCardAsDefault = useCallback(async () => {
+    const index = await ref?.current?.getActiveIndex()
+    udpatePreferences(set('variables.input.defaultCard')(cards[index]._id)({}))
+  }, [ref, cards, udpatePreferences])
 
   return (
-    <ToolbarContent title="Wallet">
+    <ToolbarContent
+      title="Wallet"
+      toolbarChildren={
+        <IonButtons slot="end">
+          <IonButton onClick={openPopover}>
+            <IonIcon className={classes.icons} icon={ellipsisVertical} />
+          </IonButton>
+        </IonButtons>
+      }
+    >
       <div className={classes.container}>
-        <IonSlides options={slideOpts}>
+        <IonSlides key={key} ref={ref} options={slideOpts}>
           {map(card => (
             <IonSlide key={card._id}>
-              <Card {...card} />
+              <Card {...card} isDefault={defaultCard === card._id} />
             </IonSlide>
-          ))(data?.cards)}
+          ))(cards)}
         </IonSlides>
       </div>
 
-      <IonModal isOpen={addCardModal}>
+      <Modal isOpen={addCardModal} onClose={toggleAddCard}>
         <NewCardView />
-      </IonModal>
+      </Modal>
 
-      <Fab onClick={toggleAddCard} />
+      <Popover event={optionsDropdown.event} onClose={closePopover}>
+        <IonItem onClick={toggleAddCard}>Add New Card</IonItem>
+        <IonItem onClick={setCardAsDefault} disabled={optionsDropdown.disableOption}>
+          Set Default Card
+        </IonItem>
+      </Popover>
     </ToolbarContent>
   )
 }
 
-export default ProfileView
+export default Wallet
