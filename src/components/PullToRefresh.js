df53f00cafaeff61614dff634f37b4bf7d4ff239
mrowe009 <mrowe009@fiu.edu>
2020-05-31 14:41:17 -0400

    fixed refresher going behind elements

--- src/components/PullToRefresh.js
+++ src/components/PullToRefresh.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { IonRefresher, IonRefresherContent } from '@ionic/react'
+import { createUseStyles } from 'react-jss'
+
+export const usePullToRefreshStyles = createUseStyles({
+  refresh: {
+    zIndex: '100 !important'
+  }
+})
 
 export const PullToRefresh = ({ onRefresh }) => {
+  const classes = usePullToRefreshStyles()
   return (
-    <IonRefresher slot="fixed" onIonRefresh={onRefresh}>
+    <IonRefresher slot="fixed" onIonRefresh={onRefresh} className={classes.refresh}>
       <IonRefresherContent />
     </IonRefresher>
   )
