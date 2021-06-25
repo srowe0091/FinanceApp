96388bc21b0acaaf9eeb84fbbbf4ee78436bcaf0
Steven Rowe <mrowe009@fiu.edu>
2021-06-24 21:47:12 -0400

    cleaning up authorized route

--- src/components/AuthorizedRoute.js
+++ src/components/AuthorizedRoute.js
@@ -4,22 +4,16 @@ import { IonPage } from '@ionic/react'
 import { Route, Redirect } from 'react-router-dom'
 
 import routes from 'routes'
-import { PAGE_ID } from 'utils'
-import { useAuthentication, useUser } from 'modules/authentication'
+import { useUser } from 'modules/authentication'
 
 export const AuthorizedRoute = ({ path, admin, component }) => {
-  const { isAuthenticated } = useAuthentication()
   const { isAdmin } = useUser()
-
-  if (!isAuthenticated) {
-    return <Redirect to={routes.login} />
-  }
 
   if (admin && !isAdmin) {
     return <Redirect to={routes.home} />
   }
 
-  return <Route path={path}>{props => <IonPage id={PAGE_ID}>{createElement(component, props)}</IonPage>}</Route>
+  return <Route path={path}>{props => <IonPage>{createElement(component, props)}</IonPage>}</Route>
 }
 
 AuthorizedRoute.propTypes = {
